b748bb2740e8145767c6dadf06a861bee778d28f
muhammedrishadpe <rishadpe@gmail.com>
2021-03-24 12:11:08 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// navbar
+
+function showMenu(event){
+        
+    event.preventDefault();
+
+    let element = document.getElementById('header');
+
+    if(element.classList.contains('active')){
+        element.className = "";
+    } else {
+        element.className = "active";
+    }
+}
+
+// navbar end
